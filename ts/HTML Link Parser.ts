88660f5c42b7cc6127bda1c6d080fae864355031
Edward Rees <erees.hk@gmail.com>
2020-09-14 03:30:12 +0800

    Removal of older files

--- ts/HTML Link Parser.ts
+++
@@ -1,6 +0,0 @@
-export const parseLink = (str: string) : string => {
-	str = str.toLowerCase();
-	let regex = /href\s*=\s*\".*\"/g 
-	let matched = str.match(regex)[0].match(/\".*\"/g)[0];
-	return matched.replace(/"/g, "");;
-}
